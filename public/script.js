562b6b57963ba6d612e090f6e0732369effeeb32
Makksaw <maxim.razumovsky@icloud.com>
2025-01-29 15:48:24 +0200

    Create other popup window with styles

--- public/script.js
+++ public/script.js
@@ -1,25 +1,27 @@
 const startQuizBtn = document.querySelector('.main-btn');
 const exitQuizBtn = document.querySelector('.popup-exit-btn');
 const continueQuizBtn = document.querySelector('.popup-continue-btn');
-const popup = document.querySelector('.popup-info');
+const popupInfo = document.querySelector('.popup-info');
+const popupQuiz = document.querySelector('.popup-quiz');
 const main = document.querySelector('.main');
 const nav = document.querySelector('.nav');
 
 startQuizBtn.addEventListener('click', () => {
-    popup.classList.add('active');
+    popupInfo.classList.add('active');
     nav.classList.add('blur');
     main.classList.add('blur');
 });
 
 exitQuizBtn.addEventListener('click', () => {
-    popup.classList.remove('active');
+    popupInfo.classList.remove('active');
     nav.classList.remove('blur');
     main.classList.remove('blur');
 });
 
 continueQuizBtn.addEventListener('click', () => {
-    popup.classList.add('fade-out');
+    popupInfo.classList.add('fade-out');
     setTimeout(() => {
-        popup.classList.remove('active', 'fade-out');
-    }, 200);
+        popupInfo.classList.remove('active', 'fade-out');
+        popupQuiz.classList.add('active', 'fade-in');
+    }, 300);
 });
